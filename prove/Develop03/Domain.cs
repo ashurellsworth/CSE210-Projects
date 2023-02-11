18590a1a1570ee04d7216d658a694871b6cba54c
ashurellsworth <ashurellsworth100@gmail.com>
2023-02-11 11:28:53 -0700

    Somewhat working Project

---
+++ prove/Develop03/Domain.cs
@@ -0,0 +1,31 @@
+using System;
+
+    public class Domain
+    {
+        private string _reference;
+        private string _scripture;
+
+        public Domain()
+        {
+            _reference = "Unknown";
+            _scripture = "Unknown";
+        }
+        public Domain(string referenceToInput)
+        {
+            _reference = referenceToInput;
+            _scripture = "";
+        }
+        public Domain(string referenceToInput, string scriptureToInput)
+        {
+            _reference = referenceToInput;
+            _scripture = scriptureToInput;
+        }
+
+        public string FraseJunta()
+        {
+            string frase = _reference + " " + _scripture;
+
+            return frase;
+        }
+
+    }
