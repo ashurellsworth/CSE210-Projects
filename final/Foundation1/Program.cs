d8ebd24f1ff3857a0527cec1880ee2a5468e86be
ashurellsworth <ashurellsworth100@gmail.com>
2023-03-25 17:31:40 -0600

    Foundation 1&2 done.

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create a list of videos
+        List<Video> videos = new List<Video>();
+
+        // Create some videos and add them to the list
+        videos.Add(new Video("Video 1", "Author 1", 60));
+        videos.Add(new Video("Video 2", "Author 2", 120));
+        videos.Add(new Video("Video 3", "Author 3", 180));
+
+        // Add comments to each video
+        videos[0].AddComment("Commenter 1", "Comment 1");
+        videos[0].AddComment("Commenter 2", "Comment 2");
+        videos[1].AddComment("Commenter 3", "Comment 3");
+        videos[2].AddComment("Commenter 4", "Comment 4");
+        videos[2].AddComment("Commenter 5", "Comment 5");
+
+        // Iterate through the list of videos and display information about each one
+        foreach (Video video in videos)
+        {
+            Console.WriteLine("Title: " + video._title);
+            Console.WriteLine("Author: " + video._author);
+            Console.WriteLine("Length: " + video._length + " seconds");
+            Console.WriteLine("Number of comments: " + video.GetCommentCount());
+
+            // Iterate through the comments for this video and display each one
+            foreach (Comment comment in video._comments)
+            {
+                Console.WriteLine("Comment by " + comment._commenter + ": " + comment._text);
+            }
+
+            Console.WriteLine(); 
+        }
+
+        Console.ReadLine(); 
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -2,8 +2,51 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation2 World!");
-    }
+        static void Main(string[] args)
+        {
+            // Create customer addresses
+            Address customerAddress1 = new Address("123 Main St", "Los Angeles", "CA", "USA");
+            Address customerAddress2 = new Address("456 Oak St", "Toronto", "ON", "Canada");
+
+            // Create customers
+            Customer customer1 = new Customer("Ashur Ellsworth", customerAddress1);
+            Customer customer2 = new Customer("Cash Ellsworth", customerAddress2);
+
+            // Create products
+            Product product1 = new Product("iPhone 13", "P001", 999.99, 1);
+            Product product2 = new Product("iPad Pro", "P002", 799.99, 2);
+            Product product3 = new Product("Apple Watch Series 7", "P003", 399.99, 1);
+
+            // Create orders
+            Order order1 = new Order(customer1);
+            order1.AddProduct(product1);
+            order1.AddProduct(product2);
+
+            Order order2 = new Order(customer2);
+            order2.AddProduct(product2);
+            order2.AddProduct(product3);
+
+            // Print packing label and shipping label for order 1
+            Console.WriteLine("Order 1 Packing Label:");
+            Console.WriteLine(order1.PackingLabel());
+            Console.WriteLine();
+
+            Console.WriteLine("Order 1 Shipping Label:");
+            Console.WriteLine(order1.ShippingLabel());
+            Console.WriteLine();
+
+            // Print packing label and shipping label for order 2
+            Console.WriteLine("Order 2 Packing Label:");
+            Console.WriteLine(order2.PackingLabel());
+            Console.WriteLine();
+
+            Console.WriteLine("Order 2 Shipping Label:");
+            Console.WriteLine(order2.ShippingLabel());
+            Console.WriteLine();
+
+            // Print total cost of order 1 and order 2
+            Console.WriteLine("Order 1 Total Cost: $" + order1.TotalCost());
+            Console.WriteLine("Order 2 Total Cost: $" + order2.TotalCost());
+            Console.WriteLine();
+        }
 }
